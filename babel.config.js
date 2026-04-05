7b5972a9cff2d7578481d0709123caeb4513190a
Asad-Kabir <asadkabir646@gmail.com>
2026-04-05 17:56:40 +0500

    intial Commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+// };
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@theme': './src/theme',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ],
 };
